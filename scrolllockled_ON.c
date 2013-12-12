1779a71262528bcc126a8257ad3785158245a6b5
truedays <ray@truedays.org>
2013-12-12 08:07:39 -0600

    hard coded C wrapper for scrollLockLED script

---
+++ scrolllockled_ON.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// CHANGE path to match your environment
+// Once compiled set the SUID bit on the binary
+
+int main()
+{
+	setuid( 0 );
+	system( "/home/ray/github/desktop/scrolllockled.sh on" );
+	return 0;
+}
