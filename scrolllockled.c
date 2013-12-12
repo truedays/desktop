3b3f325bc1e543b86070da02f19b911b70dd32f3
truedays <ray@truedays.org>
2013-12-12 07:08:02 -0600

    ScrollLockLED toggle

---
+++ scrolllockled.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	setuid( 0 );
+	system( "/home/ray/github/desktop/scrolllockled.sh" );
+	return 0;
+}
