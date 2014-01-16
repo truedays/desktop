80100445d4329f17b7b86c15c64d44521ca68126
truedays <ray@truedays.org>
2014-01-16 03:36:23 -0600

    added html2text tests

--- weather.sh
+++ weather.sh
@@ -5,6 +5,11 @@
 #
 # NOTE:  hardcored cordinates are for Arl. Hts. http://forecast.weather.gov/MapClick.php?textField1=42.1099506&textField2=-87.98247579999997
 # ToDo: remove head -n2 to get detailed report.. add this later
+
+if [ ! $(which html2text > /dev/null 2>&1) ] 
+ then echo "This script relies on html2text (http://www.mbayer.de/html2text/)"
+ exit 1
+fi
 ##while :;
 ##do
 lynx --dump "http://forecast.weather.gov/MapClick.php?lat=42.13133052651052&lon=-87.99190521240234" -source | egrep "<p class=\"myforecast-current\">.*one-third" --color | html2text | head -n2 | xargs echo | tee -a /tmp/weather.log
