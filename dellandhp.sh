d918be5df90c86a97afb4ae0f24fb2e58208ddeb
truedays <ray@truedays.org>
2014-01-20 15:58:13 -0600

    testing normal rotation

--- dellandhp.sh
+++ dellandhp.sh
@@ -3,4 +3,12 @@
 # This file gets called by ~/github/desktop/xrandr_fix.sh
 # Ray Nov 07, 2013
 
-xrandr --output VGA-1 --mode 1920x1200 --pos 2560x0 --rotate right --output DVI-I-1 --mode 2560x1440 --pos 0x224 --rotate normal --output HDMI-1 --off
+## Options:  hpright == HP monitor rotate right
+##           no args == Dell normal HP normal (default)
+
+if [ ${1} == "-hpright" ]; then
+ xrandr --output VGA-1 --mode 1920x1200 --pos 2560x0 --rotate right --output DVI-I-1 --mode 2560x1440 --pos 0x224 --rotate normal --output HDMI-1 --off
+else 
+ xrandr --output VGA-1 --mode 1920x1200 --pos 2560x0 --rotate normal --output DVI-I-1 --mode 2560x1440 --pos 0x224 --rotate normal --output HDMI-1 --off
+fi
+
--- xrandr_fix.sh
+++ xrandr_fix.sh
@@ -6,5 +6,8 @@
 /usr/bin/xrandr  --newmode "1920x1200 60" 193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
 /usr/bin/xrandr --addmode VGA-1 1920x1200
 /usr/bin/xrandr --output VGA-1 --mode 1920x1200
+
+
 # ~/github/desktop/desllandhp.sh to apply screen layout
+#~/github/desktop/dellandhp.sh -hpright
 ~/github/desktop/dellandhp.sh
