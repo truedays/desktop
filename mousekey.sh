de5f0d1aeb630b91096f4a1ec7aaece5666505a7
truedays <ray@truedays.org>
2014-07-31 20:38:36 -0500

    mousekey

--- mousekey.sh
+++ mousekey.sh
@@ -6,6 +6,10 @@
 #  **Must first enable settings >> accessibility >> Enable Mouse emulation. This solves:
 #  **Property "/MouseKeys" does not exist on channel "accessibility".
 
-[ $(xfconf-query -c accessibility -p /MouseKeys) == "false" ] \
+# Test if enabled mouse accessibility has ever been enabled and if not create the property  (CURRENTLY UNTESTED)
+##xfconf-query --list --channel accessibility 2>&1 >/dev/null || xfconf-query --create --channel accessibility --property /MouseKeys 2>&1 > /dev/null || {echo "Failed to enabled mousekeys. Are you running XFCE4"; exit 1}
+xfconf-query --list --channel accessibility 2>&1 >/dev/null || xfconf-query --create --channel accessibility --property /MouseKeys 2>&1 > /dev/null || echo "Failed to enabled mousekeys. Are you running XFCE4"
+
+[ "$(xfconf-query -c accessibility -p /MouseKeys)" == "false" ] \
 	&& $(xfconf-query -c accessibility -p /MouseKeys -s true) \
 	|| $(xfconf-query -c accessibility -p /MouseKeys -s false)
