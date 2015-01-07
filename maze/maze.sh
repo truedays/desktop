34b3b25e7918e6c8e4fe62ae854033b3022ce612
truedays <ray@truedays.org>
2015-01-07 02:00:24 -0600

    detect and mark edges in array as 1

--- maze/maze.sh
+++ maze/maze.sh
@@ -16,21 +16,40 @@ RANDOM=0 # static seed for testing
 
 # determine number of possible starting points 2 * x-axis plus 2 * y-axis
 edgeCount=$((2*$mapx+2*$mapy))
+
+# Randomize Start position limited by edgeCount
 startPos=$((${RANDOM}%${edgeCount}))
+
+##Determine edges and set start/finish point
+#for y in $(seq 0 ${mapy}); do
+# for x in $(seq 0 ${mapx}); do
+#  #basically check to see if we're at either edge..
+#  [[ $x -eq 0 || $x -eq $mapx || $y -eq 0 || $y -eq $mapy ]] && echo HIT # ${maze[$mapx+(($mapy+100))}
+#  
+# done
+#done
+
+function design_maze {
+ for y in $(seq 0 ${mapy}); do
+  for x in $(seq 0 ${mapx}); do
+  [[ $x -eq 0 || $x -eq $mapx || $y -eq 0 || $y -eq $mapy ]] && maze[${x}+$((${y}+100))]=1 && continue
+  # echo -n "${maze[$x+$y]:-0}" # echo 0 if null
+  done
+  #echo
+ done
+}
+
 
 function draw_maze {
  for y in $(seq 0 ${mapy}); do
   for x in $(seq 0 ${mapx}); do
-#   if [[ $((x + y)) -eq $startPos ]] then # detect startPos and mark
-#    echo -n "★"
-#    continue
-#   fi
-   echo -n "${maze[$x+$y]:-0}"
-#   echo -n "0"
+  [[ $x -eq 0 || $x -eq $mapx || $y -eq 0 || $y -eq $mapy ]] && echo -n X && continue
+   echo -n "${maze[$x+$(($y+100))]:-0}" # echo 0 if null
   done
   echo
  done
 }
 
+design_maze
 draw_maze
 
