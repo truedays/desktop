4629360c37c6dce74a232e278e8a5159886562e3
truedays <ray@truedays.org>
2015-01-06 22:53:25 -0600

    fixed 2d logic

--- maze/maze.sh
+++ maze/maze.sh
@@ -15,8 +15,8 @@ declare -a maze
 
 
 function draw_maze {
- for x in {0..${mapx}} do
-  for y in {0..${mapy}} do
+ for x in $(seq 0 ${mapx}); do
+  for y in $(seq 0 ${mapy}); do
    echo -n "0"
   done
   echo
