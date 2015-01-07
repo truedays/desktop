998e554a2fd97d53b50275662286348a9b181242
truedays <ray@truedays.org>
2015-01-07 00:58:10 -0600

    begin the maze array

--- maze/maze.sh
+++ maze/maze.sh
@@ -12,12 +12,21 @@
 mapx=19
 mapy=9
 declare -a maze
+RANDOM=0 # static seed for testing
 
+# determine number of possible starting points 2 * x-axis plus 2 * y-axis
+edgeCount=$((2*$mapx+2*$mapy))
+startPos=$((${RANDOM}%${edgeCount}))
 
 function draw_maze {
  for y in $(seq 0 ${mapy}); do
   for x in $(seq 0 ${mapx}); do
-   echo -n "0"
+#   if [[ $((x + y)) -eq $startPos ]] then # detect startPos and mark
+#    echo -n "★"
+#    continue
+#   fi
+   echo -n "${maze[$x+$y]:-0}"
+#   echo -n "0"
   done
   echo
  done
