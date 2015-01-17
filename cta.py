27d1346c39917638448f1eea2020a129db65646f
truedays <ray@truedays.org>
2015-01-17 00:39:27 -0600

    CTA API testing Sat Jan 17 00:39:27 CST 2015

--- cta.py
+++ cta.py
@@ -1,13 +1,23 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-apikey = "?key=KEYKEYKEY"
-apicmd = "gettime"
-apiargv = ""
+f = open('./.cta-api.key', 'r')
+apikey = "?key=" + f.read(25)
+f.close()
 
+apicmd = "getpredictions"
+apiargv = "&rt=20&stpid=456"
+
+print 'http://www.ctabustracker.com/bustime/api/v1/' + apicmd + apikey + apiargv
 r = requests.get('http://www.ctabustracker.com/bustime/api/v1/' + apicmd + apikey + apiargv)
+
+
+
+
 print r
 print r.text
-soup = BeautifulSoup(r.text)
-print soup
+#soup = BeautifulSoup(r.text)
+#print soup
 
+
+print apikey
